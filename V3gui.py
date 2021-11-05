e87a9a494606711cd81173146b73d67a1f4ad93e
Rei Arriel Clyfton <92192967+RClyf@users.noreply.github.com>
2021-11-05 19:39:32 +0700

    Add files via upload

---
+++ V3gui.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+root.title('Tugas Besar Pengkom Personal Finnancial Manager')
+
+my_label = Label(root,text='Hello World')
+
+my_label.pack()
+
+root.mainloop()
